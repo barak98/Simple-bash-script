a3b27ae001128717b4237b22fef622535a627b0f
barak98 <barak98@gmail.com>
2021-09-06 12:53:35 +0300

    all error message are shown

--- passwordvalidator.sh
+++ passwordvalidator.sh
@@ -1,24 +1,32 @@
 PASS=$@ 
+ISPASSOK=0 #i set at the start that the pass is ok
+           #but if the script finds an error it will change it to false
+           #and print all the things that are not ok
 
 #checks lenght of password
 if [ ${#PASS} -lt 10 ] ;
 then
-    echo "Password Must be at least 10 characters long" && exit 1
+    echo "Password Must be at least 10 characters long" && ISPASSOK=1
 fi
 
 #checks if ther is no an uppercase
 if ! [[  "$PASS" =~ [[:upper:]] ]]; then
-  echo "uppercase character not found" && exit 1
+  echo "uppercase character not found" && ISPASSOK=1
 fi
 
 #checks if ther is not a lowercase  
 if ! [[  "$PASS" =~ [[:lower:]] ]]; then
-  echo "lowercase character not found" && exit 1
+  echo "lowercase character not found" && ISPASSOK=1
 fi
 
 # chexkes if there is no digit
 if ! [[  "$PASS" =~ [[:digit:]] ]]; then
-  echo "digit character not found" && exit 1
+  echo "digit character not found" && ISPASSOK=1
 fi
 
-echo "pass is ok"
+if [ $ISPASSOK == 1 ]
+then
+    exit 1
+else
+    exit 0 
+fi
