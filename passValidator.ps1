a3642c8dd9557e2e19b448fb462f536f248bc2c5
barak98 <barak98@gmail.com>
2021-09-13 16:56:04 +0300

    full powershell script

--- passValidator.ps1
+++ passValidator.ps1
@@ -1,9 +1,33 @@
-$Pass = $args[0]
+$option=$args[0]
+$pass=$args[1]
+if ($option -eq "-f") {
+    if ( ! (Test-Path "./$pass" -PathType Leaf)) {
+        Write-Host "file dosent exist"
+        exit 1;
+    }
+    else {
+        $pass= Get-Content -Path "./$pass" 
+        Write-Host "$pass"
+    }
+}
+elseif (($option -eq "help") -or ($option -eq "Help") -or ($option -eq "HELP")) {
+    Write-Host "Option 1: ./passValidator.ps1 <password>"
+    Write-Host "Option 2: ./passValidator.ps1 -f <fileName.ext>    ##File MUST be in the same diractory as the script"
+    Write-Host "Password must have at least: 10 Characters, One UPPERCASE, one lowercase, and one digit "
+    exit 1;
+}
+else {
+    $pass=$args[0]
+}
+
+
+
+
 $isOk = 0
 
 if ($Pass.Length -lt 10) {
     $isOk = 1
-    Write-Host "pass should be over 10 chercters"
+    Write-Host "pass should be over 10 Characters"
 }
 
 if  ( ! ( $Pass -cmatch "[A-Z]")) {
